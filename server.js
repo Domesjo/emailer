9924dc4262a4fdaa2f41ab0a7e614ff6090b2e6b
Sam Domesjö <samdomesjo@gmail.com>
2017-05-31 15:40:42 +0100

    changes

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const morgan = require('morgan');
 const port = process.env.PORT|| 3000;
 
 const app = express();
 
+app.use(morgan());
+app.use(bodyParser());
 
 
 app.listen(3000, ()=> console.log(`express is up and running on ${port}`));
