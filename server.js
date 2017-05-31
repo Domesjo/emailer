090134d19c3d671ec8cd20f5f3a216504721f57a
Sam Domesjö <samdomesjo@gmail.com>
2017-05-31 15:55:44 +0100

    form

--- server.js
+++ server.js
@@ -8,5 +8,13 @@ const app = express();
 app.use(morgan());
 app.use(bodyParser());
 
+app.set('views', __dirname +'/index');
+app.set('view engine', 'jade');
+app.use(express.static(__dirname + '/views'));
+
+
+app.get('/', (req,res)=>{
+  res.status(200).render('index');
+});
 
 app.listen(3000, ()=> console.log(`express is up and running on ${port}`));
