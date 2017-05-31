7022382e76a6c0fa4aa3fd6018f1b4a070963153
mickyginger <mickyginger@gmail.com>
2017-05-31 15:53:23 +0100

    Added post request and nodemailer

--- server.js
+++ server.js
@@ -2,11 +2,39 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const port = process.env.PORT|| 3000;
+const compileTemplate = require('./compileTemplate');
+const nodemailer = require('bluebird').promisifyAll(require('nodemailer'));
+
+const transport = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: process.env.GMAIL_ADDRESS,
+    auth: process.env.GMAIL_PASSWORD
+  }
+});
 
 const app = express();
 
 app.use(morgan());
-app.use(bodyParser());
+app.use(bodyParser.urlendcoded({ extended: true }));
+
+app.post('/generate-email', (req, res) => {
+
+  const mailOptions = {
+    from: req.body.email,
+    to: req.body.companyEmail,
+    subject: req.body.subject,
+    text: compileTemplate(req.body)
+  };
+
+  transport
+    .sendMailSync(mailOptions)
+    .then((info) => {
+      console.log(info);
+      res.redirect('/');
+    })
+    .catch(err => res.status(500).send(err));
+});
 
 
 app.listen(3000, () => console.log(`express is up and running on ${port}`));
